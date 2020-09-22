44df33d4da9916b78eea3bc67db53c6e3c83e1fb
Pierce Rogg <roggpierce@gmail.com>
2020-09-22 17:23:01 -0500

    Fixed html and javaScript links

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const characterAmountRange = document.getElementById('characterAmountRange');
 const characterAmountNumber =document.getElementById('characterAmountNumber');
 const includeUppercaseElement = document.getElementById('includeUppercase')
 const includeNumbersElement = document.getElementById('includeNumbers')
-const includeUppercaseElement = document.getElementById('includeSymbols')
+const includeSymbolsElement = document.getElementById('includeSymbols')
 const form = document.getElementById('passwordGeneratorForm');
 const passwordDisplay = document.getElementById('passwordDisplay')
 
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
-const LOWERRCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
 const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
     arrayFromLowToHigh(58, 64)
@@ -26,16 +26,22 @@ form.addEventListener('submit', e => {
     const includeUppercase = includeUppercaseElement.checked;
     const includeNumbers = includeNumbersElement.checked;
     const includeSymbols = includeSymbolsElement.checked;
-    const password =generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols);
+    const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols);
     passwordDisplay.innerText = password;
 })
 
 function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
     let charCodes = LOWERCASE_CHAR_CODES;
-    if (includeUppercase) charCodes = charCodes.concat(UPPERRCASE_CHAR_CODES);
-    if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES);
-    if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES);
-    
+    if (includeUppercase) {
+        charCodes = charCodes.concat(UPPERCASE_CHAR_CODES);
+    }
+    if (includeNumbers) {
+        charCodes = charCodes.concat(NUMBER_CHAR_CODES);
+    }
+    if (includeSymbols) {
+        charCodes = charCodes.concat(SYMBOL_CHAR_CODES);
+    }
+
     const passwordCharacters =[];
     for (let i = 0; i < characterAmount; i++) {
         const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)];
